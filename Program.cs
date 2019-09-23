60b9a16bfca0b7d83e65bd7b039eaff08eb1f956
Chinmayi Karmalkar <CAKarmalkar@tecoenergy.com>
2019-09-22 23:32:35 -0400

    All changes done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿/*
  * Author: Chinmayi Karmalkar 
  * Date: 09/19/2019
- * Comments: This C# Console application consists of the solution to Assignment1
+ * Comments: This C# Console application consists of the solution to Assignment1 which makes 
+ *          use of methods, arrays and loops.
  */
 
 
 
 
-using System;   // ADD COMMENTS WHERE NEEDED
+using System;   
+using System.Linq;
 
 namespace Assignment1
 {
@@ -18,45 +20,72 @@ namespace Assignment1
             Program p = new Program();
 
             //1. Self Dividing Numbers
-            //Console.WriteLine("1. Self Dividing Numbers");
-            //int a = 1, b = 22;
-            //p.selfDividingNumbers(a, b);
+            Console.WriteLine("\n1. Self Dividing Numbers");
+            int a = 1, b = 22;
+            p.selfDividingNumbers(a, b);
 
             //2. Print Series
-            //Console.WriteLine("2. Print Series");
-            //p.series(5);
+            Console.WriteLine("\n2. Print Series");
+            p.series(5);
 
             //3. Print Inverse Triangle
-            //Console.WriteLine("3. Print inverse Triangle");
-            //int n1=5;
-            //p.printTriangle(n1);
+            Console.WriteLine("\n3. Print inverse Triangle");
+            int n1 = 5;
+            p.printTriangle(n1);
 
             //4. Count number of jewels in stones
-            //Console.WriteLine("4. Count number of jewels in stones");
-            //int[] j= { 1, 3 };
-            //int[] s= { 1, 3, 3, 2, 2, 2, 2, 2 };
-            //int final=numJewelsInStone(j,s);
-            //Console.WriteLine(final);
+            Console.WriteLine("\n4. Count number of jewels in stones");
+            int[] j = { 1, 3 };
+            int[] s = { 1, 3, 3, 2, 2, 2, 2, 2 };
+            int final = numJewelsInStone(j, s);
+            Console.WriteLine(final);
 
             //5.Longest Sub Array
-            Console.WriteLine("5. Longest Sub Array");
+            Console.WriteLine("\n5. Longest Sub Array");
             int[] arr1 = new int[] { 1, 5, 3, 4, 6, 7, 9 };
             int[] arr2 = new int[] { 6, 3, 5, 4, 2 };
-            //displayArray(arr1);
-
-            //array to get the largest common sub array
             getLargestCommonSubArray(arr1, arr2);
 
             //6. Solve Puzzle
-            //Console.WriteLine("6. Solve Puzzle");
-            //solvePuzzle();
+            Console.WriteLine("\n6. Solve Puzzle");
+            solvePuzzle();
 
             Console.ReadLine();
         }
 
-        /*void printTriangle(int n)
+        void selfDividingNumbers(int x, int y)
         {
-            try
+            try //try block begins
+            {
+                for (int i = x; i <= y; i++)
+                {
+                    int r1 = isSelfDividing(i);
+                    if (r1 == 1)
+                        Console.WriteLine(i);
+                }
+                int isSelfDividing(int n)
+                {
+                    int temp = n;
+                    while (temp > 0)
+                    {
+                        int d = temp % 10;
+                        if (d == 0 || n % d != 0)
+                            return 0;
+                        temp = temp / 10;
+                    }
+                    return 1;
+                }
+
+            } //try block ends
+            catch //catch block begins
+            {
+                Console.WriteLine("Exception occured while running selfDividingNumbers()");
+            } // catch block ends
+        }
+
+        void printTriangle(int n)
+        {
+            try //try block begins
             {
                 int i, k, count = 1;
                 //int num = 5;
@@ -69,187 +98,188 @@ namespace Assignment1
                         Console.Write("*");
                     Console.WriteLine();
                 }
-            }
-            catch
+            } //try block ends
+            catch // catch block begins
             {
                 Console.WriteLine("Exception while computing PrintTriangle()");
-            }
-        }*/
+            } //catch block ends
+        }
 
-        //void selfDividingNumbers(int x, int y)
-        //{
-        //    try
-        //    {
-        //        for (int i = x; i <= y; i++)
-        //        {
-        //            int r1 = isSelfDividing(i);
-        //            if (r1 == 1)
-        //                Console.WriteLine(i);
-        //        }
-        //        int isSelfDividing(int n)
-        //        {
-        //            int temp = n;
-        //            while (temp > 0)
-        //            {
-        //                int d = temp % 10;
-        //                if (d == 0 || n % d != 0)
-        //                    return 0;
-        //                temp = temp / 10;
-        //            }
-        //            return 1;
-        //        }
+        void series(int n)
+        {
+            try //try block begins
+            {
+                //loop to increment numbers 1 to 5
+                for (int i = 1; i <= n; i++)
+                {
+                    //loop to print the number equal to its value
+                    for (int j = 1; j <= i; j++)
+                        Console.WriteLine(i);
+                }
+            } //try block ends
+            catch //catch block begins
+            {
+                Console.WriteLine("Exception occured while running Series()");
+            } //catch block ends
 
-        //    }
-        //    catch
-        //    {
-        //        Console.WriteLine("Exception occured while running selfDividingNumbers()");
-        //    }
-        // }
+        }
 
-        //void series(int n)
-        //{
-        //    try
-        //    {
-        //        //loop to increment numbers 1 to 5
-        //        for (int i = 1; i <= n; i++)
-        //        {
-        //            //loop to print the number equal to its value
-        //            for (int j = 1; j <= i; j++)
-        //                Console.WriteLine(i);
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        Console.WriteLine("Exception occured while running Series()");
-        //    }
+        static int numJewelsInStone(int[] a, int[] b)
+        {
+            try //try block begins
+            {
+                int count = 0;
+                //loop for jewels array
+                for (int i = 0; i < b.Length; i++)
+                {
+                    //loop for stones array
+                    for (int j = 0; j < a.Length; j++)
+                    {
+                        //compare to check for common jewels in stones
+                        if (b[i] == a[j])
+                        {
+                            Console.WriteLine(b[i]);
+                            count++;
+                        }
 
-        //}
-
-        //static int numJewelsInStone(int[] a, int[] b)
-        //{
-        //    try
-        //    {
-        //        int count = 0;
-        //        loop for jewels array
-        //        for (int i = 0; i < b.Length; i++)
-        //            {
-        //                loop for stones array
-        //                for (int j = 0; j < a.Length; j++)
-        //                    {
-        //                        compare to check for common jewels in stones
-        //                        if (b[i] == a[j])
-        //                            {
-        //                                Console.WriteLine(b[i]);
-        //                                count++;
-        //                            }
-
-        //                    }
-        //            }
-        //        return count;
-        //    }
-        //    catch
-        //    {
-        //        Console.WriteLine("Exception occured while computing numJewelsInStones()");
-        //    }
-        //    return 0;
-        //}
+                    }
+                }
+                return count;
+            } //try block ends
+            catch //catch block begins
+            {
+                Console.WriteLine("Exception occured while computing numJewelsInStones()");
+            } //catch block ends
+            return 0;
+        }
 
         public static void getLargestCommonSubArray(int[] m, int[] n)
         {
-            int min_position; //to keep of lowest element position
-            int temp; //for swap
-
-            //sort array1 - Selection Sort
-            //Console.WriteLine("Array 1 Sorted");
-            for (int i=0; i<m.Length;i++)
+            try //try block begins
             {
-                min_position = i;
+                int min_position; //to keep of lowest element position
+                int temp; //for swap
 
-                for(int j=i+1; j<n.Length;j++)
+                //sort array1 - Selection Sort
+                //Console.WriteLine("Array 1 Sorted");
+                for (int i = 0; i < m.Length; i++)
                 {
-                    if (m[j]<m[min_position])
+                    min_position = i;
+
+                    for (int j = i + 1; j < n.Length; j++)
                     {
-                        min_position =j;
+                        if (m[j] < m[min_position])
+                        {
+                            min_position = j;
+                        }
                     }
-                }
 
-                if(min_position!=i)
-                {
-                    temp = m[i];
-                    m[i] = m[min_position];
-                    m[min_position] = temp;
-                }
-                //Console.Write(" " + m[i]);
-            }
-
-            //Console.WriteLine("\n Array 2 sorted ");
-
-            //sort array2 - Selection Sort
-            for (int i = 0; i < n.Length; i++)
-            {
-                min_position = i;
-
-                for (int j = i + 1; j < n.Length; j++)
-                {
-                    if (n[j] < n[min_position])
+                    if (min_position != i)
                     {
-                        min_position = j;
+                        temp = m[i];
+                        m[i] = m[min_position];
+                        m[min_position] = temp;
                     }
+                    //Console.Write(" " + m[i]);
                 }
 
-                if (min_position != i)
+                //Console.WriteLine("\n Array 2 sorted ");
+
+                //sort array2 - Selection Sort
+                for (int i = 0; i < n.Length; i++)
                 {
-                    temp = n[i];
-                    n[i] = n[min_position];
-                    n[min_position] = temp;
+                    min_position = i;
+
+                    for (int j = i + 1; j < n.Length; j++)
+                    {
+                        if (n[j] < n[min_position])
+                        {
+                            min_position = j;
+                        }
+                    }
+
+                    if (min_position != i)
+                    {
+                        temp = n[i];
+                        n[i] = n[min_position];
+                        n[min_position] = temp;
+                    }
+                    //Console.Write(" " + n[i]);
                 }
-                //Console.Write(" " + n[i]);
-            }
 
-            int lim;
-            //Console.WriteLine("\n***");
-            //checking lengths of the arrays
-            if (m.Length < n.Length)
-                lim = m.Length;
-            else
-                lim = n.Length;
-
-            //array to store the final values of the common elements from both arrays
-            int[] final = new int[10];
-
-            for (int k=0;k<lim;k++)
-            {
-                if (m[k] == n[k])
-                    final[k] = m[k];
-            }
-
-            Console.WriteLine("\n\nFinal Array");
-            for (int l = 0; l < final.Length; l++)
-            {
-                if (final[l] == 0)
-                    continue;
+                int lim;
+                //Console.WriteLine("\n***");
+                //checking lengths of the arrays
+                if (m.Length < n.Length)
+                    lim = m.Length;
                 else
-                    Console.Write(" " + final[l]);
+                    lim = n.Length;
+
+                //array to store the final values of the common elements from both arrays
+                int[] final = new int[10];
+
+                for (int k = 0; k < lim; k++)
+                {
+                    if (m[k] == n[k])
+                        final[k] = m[k];
+                }
+
+                Console.WriteLine("\n\nFinal Array");
+                for (int l = 0; l < final.Length; l++)
+                {
+                    if (final[l] == 0)
+                        continue;
+                    else
+                        Console.Write(" " + final[l]);
+                }
+            } //try block ends
+            catch //catch block begins
+            {
+                Console.WriteLine("Exception occured while running getLongestCommonSubArray()");
+            } //catch block ends
+
+        }
+
+        public static void displayArray(int[] a)
+        {
+
+        }
+
+
+
+        public static void solvePuzzle()
+        {
+            try
+            {
+                //input strings -ask for input string and output strings - method to collect inputs
+                getinputoutput();
+
+                //method to identify unique characters in the string
+
+                //method to test assigned values
+
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while running solvePuzzle()");
             }
         }
 
-        //public static void displayArray(int[] a)
-        //{
-            
-        //}
+        public static string getinputoutput()
+        {
+            string input1, input2, output;
+            Console.WriteLine("solving the puzzle method");
+            Console.WriteLine("\n enter the first string: ");
+            input1 = Console.ReadLine();
 
+            Console.WriteLine("\nenter second string: ");
+            input2 = Console.ReadLine();
 
+            Console.WriteLine("\nenter the output string: ");
+            output = Console.ReadLine();
 
-        //void solvePuzzle()
-        //{
-        //    //input strings - ask for input string and output strings - method to collect inputs
-
-        //    //method to identify unique characters in the string
-
-        //    //method to test assigned values 
-
-        //    //return void
-        //}
+            return output;
+        }
     }
 }
 
